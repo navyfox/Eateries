62f13623e551d9cf316e0ae8f4e418488fb44192
Игорь Михайлович Ракитянский <i697@ya.ru>
2016-11-01 22:15:40 +0600

    Custom table view cell

---
+++ Eateries/EateriesTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  EateriesTableViewCell.swift
+//  Eateries
+//
+//  Created by Игорь Михайлович Ракитянский on 01.11.16.
+//  Copyright © 2016 RIM. All rights reserved.
+//
+
+import UIKit
+
+class EateriesTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
