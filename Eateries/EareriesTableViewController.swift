4c55fab9bb280c5aae4b492475f7779b1d6cf5be
Игорь Михайлович Ракитянский <i697@ya.ru>
2016-11-08 00:12:53 +0600

    Passing data between scenes

--- Eateries/EareriesTableViewController.swift
+++ Eateries/EareriesTableViewController.swift
@@ -103,6 +103,15 @@ class EareriesTableViewController: UITableViewController {
         return [delete, share]
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "detailSegue" {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let destinationViewController = segue.destination as! EateryDetailViewController
+                destinationViewController.imageName = self.restaurantImages[indexPath.row]
+            }
+        }
+    }
+
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
